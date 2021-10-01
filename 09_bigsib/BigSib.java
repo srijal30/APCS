fa21b51f3adf561cc1085437e0bc107629a27b25
Salaj Rijal <srijal30@stuy.edu>
2021-10-01 15:18:12 -0400

    yo

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,29 @@
+/*
+Ubuntus: Salaj: George, May: Perry Jr., Faiyaz: Atlas and Payne
+APCS
+HW 09 -- What are BigSibs Good For? -- How Java Files interact
+2021-10-01
+
+DISCOVERIES:
+-you need the keyword stati
+-you cant use the java command to run a file if there is a class verision of itc
+
+UNRESOLVED QUESTIONS:
+-why do you need public?
+-why did for one of my team memebers, their folder got turned into a file?
+-why do you need static?
+-why cant u use java command after you use javac command?
+--ERROR MESSAGE:
+----error: class found on application class path: Greet
+0
+
+*/
+public class BigSib{
+	public static void main(String[] args){
+	}
+	public static void greet(String str){
+		System.out.println("Yo whats up " + str + ", did you do the CS homework?");
+
+	}
+}
+
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,18 @@
+/*
+Ubuntus: Salaj: George, May: Perry Jr., Faiyaz: Atlas and Payne
+APCS
+HW 09 -- What are BigSibs Good For? -- How Java Files interact
+2021-10-01
+
+DISCOVERIES:
+
+UNRESOLVED QUESTIONS:
+
+*/
+public class Greet {
+        public static void main(String[] args){
+              BigSib.greet("Daniel");
+              BigSib.greet("Raj");
+              BigSib.greet("Caleb");
+        }
+}
