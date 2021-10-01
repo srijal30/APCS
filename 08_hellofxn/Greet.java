a9a858d3c35c80707badb3541bc5d67ab655b816
Salaj Rijal <srijal30@stuy.edu>
2021-09-30 22:11:11 -0400

    wassup 2

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -5,13 +5,16 @@ HW 08 -- Refractor Freshie Zero -- Custom Function
 2021-09-30
 
 DISCOVERIES:
--you need the keyword static
+-you need the keyword stati
+-you cant use the java command to run a file if there is a class verision of itc
 
 UNRESOLVED QUESTIONS:
 -why do you need public?
 -why did for one of my team memebers, their folder got turned into a file?
 -why do you need static?
 -why cant u use java command after you use javac command?
+--ERROR MESSAGE:
+----error: class found on application class path: Greet
 0
 
 */
