4663a01678fc092d69b8c12402bbda5951a2adcb
Salaj Rijal <srijal30@stuy.edu>
2021-10-04 18:06:47 -0400

    fix to the hw

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -5,17 +5,8 @@ HW 10 -- Refactor Big Sib One -- Make the method return a value
 2021-10-04
 
 DISCOVERIES:
--you need the keyword stati
--you cant use the java command to run a file if there is a class verision of itc
 
 UNRESOLVED QUESTIONS:
--why do you need public?
--why did for one of my team memebers, their folder got turned into a file?
--why do you need static?
--why cant u use java command after you use javac command?
---ERROR MESSAGE:
-----error: class found on application class path: Greet
-0
 
 */
 public class BigSib{
