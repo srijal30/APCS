4d9cc69be618b915ac98325d030fd1c3911405e1
Salaj Rijal <srijal30@stuy.edu>
2021-10-04 18:05:25 -0400

    homework 10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,29 @@
+/*
+Ubuntus: Salaj: George, May: Perry Jr., Faiyaz: Atlas and Payne
+APCS
+HW 10 -- Refactor Big Sib One -- Make the method return a value
+2021-10-04
+
+DISCOVERIES:
+-you need the keyword stati
+-you cant use the java command to run a file if there is a class verision of itc
+
+UNRESOLVED QUESTIONS:
+-why do you need public?
+-why did for one of my team memebers, their folder got turned into a file?
+-why do you need static?
+-why cant u use java command after you use javac command?
+--ERROR MESSAGE:
+----error: class found on application class path: Greet
+0
+
+*/
+public class BigSib{
+	public static void main(String[] args){
+	}
+	public static String greet(String str){
+		return "Yo whats up " + str + ", did you do the CS homework?";
+
+	}
+}
+
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,18 @@
+/*
+Ubuntus: Salaj: George, May: Perry Jr., Faiyaz: Atlas and Payne
+APCS
+HW 10 -- Refractor Big Sib One -- Making the function return a value 
+2021-10-04
+
+DISCOVERIES
+
+UNRESOLVED QUESTIONS:
+
+*/
+public class Greet {
+        public static void main(String[] args){
+             System.out.println(BigSib.greet("Daniel"));
+             System.out.println(BigSib.greet("Raj"));
+             System.out.println(BigSib.greet("Caleb"));
+        }
+}
