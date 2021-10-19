126093690c7b9a1b2aa8fa730cb223716fd6e956
Salaj Rijal <srijal30@stuy.edu>
2021-10-18 20:05:41 -0400

    yo

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -1,21 +1,22 @@
 /*
 Triumphant Triumvirate: Salaj Rijal: George, Ariel Fuchs: Skelly Ducky, Ben Belotser: TwoFace
 APCS
-HW 18 -- CPA-One -- Creating/Testing BankAccount.java with the help of UML Diagram
-2021-10-16
+HW 19 -- Mo Money Mo Problems... MORE AWESOME -- Creating authenticate() with the help of conditionals
+2021-10-18
 
 DISCO:
--The initial value for int is 0, null for string, 0.0 for double
--
+
 QCC:
--Should balance be set up in overcon + have a SetBalance() method?
--How does java know what constructor to use?
--what is a good name for inputVariables?
--should variables be initialized in default constructor or in the class?
--is it necassary to add an if statement to limit input params?
+
 Q2:
+You know that java creates a default constructor becasue if there are not constructors defined in a class and you try to construct it, an instance of the class is still returned. An example of this default constructor is:
+public classNam(){
+	//no parameters
+}
 
 Q3:
+BigSib richard = new BigSib();
+System.out.println(richard); //Output: "BankAccount@49476842"
 
 */
 
@@ -24,62 +25,60 @@ public class BankAccount{
 	//Intitialization
 	private String name;
 	private String password;
-	private int pin;
+	private short pin;
 	private int accountNumber;
-	private double balance = 0.0;
+	private double balance;
 
-	//DefaultConstructor
-	public BankAccount(){
+	public String toString() {
+		String retStr = "\nAccount info:\n=======================";
+      		retStr = retStr + "\nName: " + name;
+      		retStr = retStr + "\nPassword: " + passwd;
+      		retStr = retStr + "\nPIN: " + pin;
+      		retStr = retStr + "\nAccount Number: " + acctNum;
+      		retStr = retStr + "\nBalance: " + balance;
+      		retStr = retStr + "\n=======================";
+      		return retStr;
+	}
+ 	
+	//mutators
+	public String setName( String newName ) {
+  		String oldName = name;
+		name = newName;
+		return oldName;
+	}
+	
+	public String setPasswd( String newPasswd ) {
+		String oldPasswd = passwd;
+		passwd = newPasswd;
+		return oldPasswd;
+	}
+	
+	public short setPin( short newPin ) {
+		short oldPin = pin;
+		pin = newPin;
+		return oldPin;
 	}
 
-	//OverloadedConstructor
-	public BankAccount(String _name, String _pass, int _pin, int _accnt){
-		name = _name;
-		password = _pass;
-		pin = _pin;
-		accountNumber = _accnt;
+	public int setAcctNum( int newAcctNum ) {
+		int oldAcctNum = acctNum;
+		acctNum = newAcctNum;
+		return oldAcctNum;
 	}
 
-	//ShowInfo() method
-	public void ShowInfo(){
-		System.out.println(name);
-		System.out.println(password);
-		System.out.println(pin);
-		System.out.println(accountNumber);
-		System.out.println(balance);
-		System.out.println();
-	}
-
-	//Set..() methods
-	public void SetName(String _name){
-		name = _name;
+	public double setBalance( double newBalance ) {
+		double oldBalance = balance;
+		balance = newBalance;
+		return oldBalance;
 	}	
-
-	public void SetPass(String _pass){
-		password = _pass;
-	}	
-
-	public void SetPin(int _pin){
-		pin = _pin;
-	}	
-
-	public void SetAccount(int _account){
-		accountNumber = _account;
-	}
-
-	//ShowBalance() Method //NOT NECASSARY BUT HELPFUL FOR TESTING
-	public void ShowBalance(){
-		System.out.println(balance);
-	}
 
 	//Balance Affecting Methods
 	//Withdraw	
-	public void Withdraw(double amount){
+	public void withdraw(double amount){
 		balance -= amount;
 	}
 
 	//Deposit
-	public void Deposit(double amount){
+	public void deposit(double amount){
 		balance += amount;
 	}
 
@@ -110,5 +109,8 @@ public class BankAccount{
 		elonMusk.ShowBalance();
 		elonMusk.Withdraw(-1.21);	
 		elonMusk.ShowBalance();
+
+		//Test for string representation without .toString()
+		System.out.println(elonMusk);
 	}
 }
