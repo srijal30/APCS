72e706620040f09bb32c37952808aecce79943ef
Salaj Rijal <60166498+srijal30@users.noreply.github.com>
2021-10-24 19:23:53 -0400

    Delete Test.java

--- 23_Coin/Test.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.Random;
-public class Test {
-    public static void main(String[] args) {
-        Random rng = new Random();
-        System.out.println(rng.nextDouble());
-    }
-    
-}
