af16c397fc8f350f1a347fc587900d6e9592cd77
srijal30 <srijal30@stuy.edu>
2021-11-13 21:41:10 -0500

    accepted

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -1,27 +1,36 @@
 public class Loopier{
 
-  public static void printArray( int[] array){
+  public static String arrayToString( int[] array){
+    /**
+     * converts an int array to a string
+     */
     String result = "[";
     for(int number: array){
       result += Integer.toString(number) + ", ";
     }
-    result = result.substring(0, result.length() - 2) + "]";
-    System.out.println(result);
+    result = result.substring(0, result.length()-2) + "]";
+    return result;
   }
 
   public static void populate( int[] array ){
+    /**
+     * populates an int array with random integers
+     */
     for(int i=0; i < array.length; i = i + 1){
-        array[i] = Math.random() * Math.pow(2, 32);
+        array[i] = (int) ( Math.random() * Math.pow(2, 32) );
     }
   }
 
+
+
   public static void main(String[] args) {
+    //test of arrayToString
     int[] michael = {1, 2, 3, 4, 5, 6, 7, 8, 69};
-    printArray( michael);
+    System.out.println( arrayToString(michael) );
 
-    int several = 10;
-    int[] salaj = new int[several];
-
-    populate(salaj)
+    //test of populate
+    int[] salaj = new int[10];
+    populate(salaj);
+    System.out.println( arrayToString(salaj) );
   }
 }
