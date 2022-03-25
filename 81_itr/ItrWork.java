9ac9269551d094bcc2ece1e8b7d6c6d98d77c191
srijal30 <srijal30@stuy.edu>
2022-03-24 21:52:19 -0400

    twerking thursdays

--- 81_itr/ItrWork.java
+++ 81_itr/ItrWork.java
@@ -1,3 +1,9 @@
+//Froghats: Kevin Cheng, Salaj Rijal, Alif Rahman
+//APCS pd8
+//HW81 -- Thank You, Next -- twerking w/ iterators
+//2022-03-24r
+//time spent: 1h
+
 /***
  * 
  *  SKELETON
@@ -5,13 +11,24 @@
  **/
 
 /***
-    DISCO
-
-    QCC
+    DISCO:
+    - An object that implements Iterator cannot be initialized from a constructor since Iterator is an interface. You have to use a method
+    called iterator() that is in all collection classes.
+  
+    QCC:
+    - What exactly is an Iterator? Is it it's own object? How is it related to the collection it is iterating? Modifying it in the removeEvens() 
+    method also modifies List<Integer> L.
 
     SUMMARY THE FIRST:
+    all classes that implement the iterator interface must have these method signatures:
+    - public boolean hasNext()
+    - public Object next()
+    - public void remove()
 
     SUMMARY THE SECOND:
+    create methods that find a key in a list of integers, returns list of odd numbers and removes all even numbers in a 
+    list. For the ones where we check if a key is in a list, and return a odd number list, we have to create implementations with
+    for each as well.
 
 **/
 
@@ -28,7 +45,7 @@ public class ItrWork
   public static boolean foundA( Integer key, List<Integer> L )
   {
     for (Integer i : L) {
-        if( i == key){
+        if( i.equals(key) ){
             return true;
         }
     }
@@ -39,74 +56,93 @@ public class ItrWork
   //returns a boolean to indicate whether key is present in L
   public static boolean foundB( Integer key, List<Integer> L )
   {
-      Iterator l = L.iterator();
-      while( l.hasNext() ){
-          if ( l.next().equals(key) ) return true;
+    Integer current;
+    Iterator<Integer> i = L.iterator();
+    while ( i.hasNext() ) {
+      current = i.next();
+      if (current.equals(key)) {
+        return true;
       }
-      return false;
     }
+    return false;
+  }
 
   //using FOREACH loop
   //returns a list containing the odd numbers in L
   public static List<Integer> oddsA( List<Integer> L )
   {
-    /*** YOUR IMPLEMENTATION HERE ***/
+    List<Integer> result = new ArrayList<Integer>();
+    for( Integer i: L ){
+      if( i%2==1 ) result.add(i);
+    }
+    return result;
   }
 
   //explicitly using an iterator
   //returns a list containing the odd numbers in L
   public static List<Integer> oddsB( List<Integer> L )
   {
-    /*** YOUR IMPLEMENTATION HERE ***/
+    List<Integer> result = new ArrayList<Integer>();
+    Iterator<Integer> i = L.iterator();
+    Integer element;
+    while ( i.hasNext() ){
+      element = i.next();
+      if ( element%2==1 ) {
+        result.add(element);
+      }
+    }
+    return result;
   }
-
 
   //explicitly using an iterator
   //modifies L s.t. it contains no evens
   public static void removeEvens( List<Integer> L )
   {
-    /*** YOUR IMPLEMENTATION HERE ***/
+    Iterator<Integer> itr = L.iterator();
+    while( itr.hasNext() ){
+      if( itr.next()%2==0 ) itr.remove(); 
+    }
   }
-
 
   public static void main( String [] args )
   {
-    /*~~~~~~~~~~~~~~~m~o~v~e~~m~e~~d~o~w~n~~~~~~~~~~~~~~
-
+    
     //var type: List   obj type: ?
-    List<Integer> L =
-
+    List<Integer> L = new ArrayList<Integer>();
     for( int i = 0; i < 10; i++ )
-      L.add(i);
-
-
+    L.add(i);
+    
     // TASK: write code to print the contents of L...
-
     // a) using a FOREACH loop
-
-
+    for( Integer i: L) System.out.println(i);
+    System.out.println();
     // b) explicitly using an iterator
+    Iterator<Integer> itr = L.iterator();
+    while( itr.hasNext() ){
+      System.out.println( itr.next() );
+    }
 
-
+    
     System.out.println("\nTesting foundA...");
     System.out.println("9 in L? -> " + foundA(9,L) );
     System.out.println("13 in L? -> " + foundA(13,L) );
-
+    
     System.out.println("\nTesting foundB...");
     System.out.println("9 in L? -> " + foundB(9,L) );
     System.out.println("13 in L? -> " + foundB(13,L) );
-
+    
     System.out.println("\nTesting oddsA...");
     List<Integer> A = oddsA(L);
     for( int n : A ) System.out.println(n);
-
+    
     System.out.println("\nTesting oddsB...");
     List<Integer> B = oddsB(L);
     for( int n : B ) System.out.println(n);
-
+    
     System.out.println("\nTesting removeEvens...");
     removeEvens(L);
     for( int n : L ) System.out.println(n);
+    /*~~~~~~~~~~ ~~~~~m~o~v~e~~m~e~~d~o~w~n~~~~~~~~~~~~~~
       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
   }//end main
