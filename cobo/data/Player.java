a7164709d9ec066801ca14fd0af94954df442c6b
Salaj Rijal <srijal30@stuy.edu>
2022-04-06 16:08:27 -0400

    stuff

--- cobo/data/Player.java
+++ cobo/data/Player.java
@@ -1,26 +1,21 @@
 public class Player{
 
     private String name;
-    private String ptsPerGame;
+    private double points;
     
 
     public Player( String name, String pts){
         this.name = name;
-        ptsPerGame = pts; 
+        points = Double.parseDouble(pts); 
     }
 
     public String toString(){
-        return "Name: " + name + "\nPPG: " + ptsPerGame;
-    }
-
-    public double points(){
-        return Double.parseDouble( ptsPerGame );
-
+        return "Name: " + name + "\nPPG: " + Double.toString(points);
     }
 
     public int compareTo(Player x) {
-        if (this.points() > x.points()) return 1;
-        else if ( x.points() > this.points() ) return -1;
+        if ( this.points > x.points) return 1;
+        else if ( x.points > this.points ) return -1;
         return 0;
     }
 
