7c7214ca0dbbb140b0fe792aade34818211b7e1b
srijal30 <srijal30@stuy.edu>
2022-04-11 22:24:32 -0400

    stuff

---
+++ cobo/data/MVPFinder.java
@@ -0,0 +1,20 @@
+public class MVPFinder {
+
+    public static void main(String[] args) {
+
+        DataSource ds = DataSource.connect("players.xml").load();
+        ds.printUsageString();
+        ArrayList<Player> players = ds.fetchList("Player", "row/Player", "row/PTS" );
+
+        Player best = players.get(0);
+
+        for( Player p: players ){
+
+            if( p.compareTo(best) > 0 ) best = p;
+        }
+
+        System.out.println( best );
+
+    }
+
+}
